3a65e4657e43cedd40eee599db9b24001f2918c5
Kostya Shvets <shostyyyy@yandex.ru>
2022-08-12 14:23:29 +0300

    fix: исправить ошибки в JS, улучшить семантику кода

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,28 @@
 const popup = document.querySelector('.popup');
-const openButton = document.querySelector('.edit-button');
-const closeButton = document.querySelector('.close-button');
+const openButton = document.querySelector('.profile__edit-button');
+const closeButton = popup.querySelector('.popup__close-button');
 let popupForm = popup.querySelector('.popup__form');
+let profileName = document.querySelector('.profile__name');
+let profileBio = document.querySelector('.profile__bio');
+const saveButton = popup.querySelector('.popup__save-button');
 
-const togglePopup = function () {
-  popup.classList.toggle('popup_closed')
+const openPopup = function () {
+  popup.classList.add('popup_opened')
 }
 
-openButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
-
-
-const saveButton = document.querySelector('.save-button');
+const closePopup = function () {
+  popup.classList.remove('popup_opened')
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
   const nameInput = popup.querySelector('.popup__name').value;
   const bioInput = popup.querySelector('.popup__bio').value;
-
-  let profileName = document.querySelector('.profile__name');
-  let profileBio = document.querySelector('.profile__bio');
-
   profileName.textContent = nameInput;
   profileBio.textContent = bioInput;
-  togglePopup();
+  closePopup();
 }
 
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
