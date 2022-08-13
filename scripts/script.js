694980901390d038b43f16b303fa57958989783e
Kostya Shvets <shostyyyy@yandex.ru>
2022-08-13 18:33:31 +0300

    fix: исправить замечания от первого ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,30 @@
-const popup = document.querySelector('.popup');
-const openButton = document.querySelector('.profile__edit-button');
-const closeButton = popup.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = popup.querySelector('.popup__close-button');
 let popupForm = popup.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let profileBio = document.querySelector('.profile__bio');
-const saveButton = popup.querySelector('.popup__save-button');
+let popupSaveButton = popup.querySelector('.popup__save-button');
+let nameInput = popup.querySelector('.popup__input_data_name');
+let bioInput = popup.querySelector('.popup__input_data_bio');
 
 const openPopup = function () {
-  popup.classList.add('popup_opened')
+  popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  bioInput.value = profileBio.textContent;
 }
 
 const closePopup = function () {
-  popup.classList.remove('popup_opened')
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  const nameInput = popup.querySelector('.popup__name').value;
-  const bioInput = popup.querySelector('.popup__bio').value;
-  profileName.textContent = nameInput;
-  profileBio.textContent = bioInput;
+  profileName.textContent = nameInput.value;
+  profileBio.textContent = bioInput.value;
   closePopup();
 }
 
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', formSubmitHandler);
