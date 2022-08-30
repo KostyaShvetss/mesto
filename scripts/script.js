8c828d45c77c5b5cf4a7a785616227517f1bd5b7
Kostya Shvets <shostyyyy@yandex.ru>
2022-08-30 12:37:19 +0300

    refactor: исправить код, улучшить читаемость

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,29 @@
 // ПЕРЕМЕННЫЕ
-const popup = document.querySelector('.popup');
+// Попапы
+const profilePopup = document.querySelector('.profile-popup');
+const editingPopup = document.querySelector('.edit-popup');
+const ImagePopup = document.querySelector('.popup-image');
+// Кнопки
 const profileEditButton = document.querySelector('.profile__edit-button');
-const popupCloseButton = popup.querySelector('.popup__close-button');
-const popupForm = popup.querySelector('.popup__form');
+const closeButtons = document.querySelectorAll('.popup__close');
+console.log(closeButtons);
+const profileAddButton = document.querySelector('.profile__add-button');
+// Формы и инпуты
+const popupForm = profilePopup.querySelector('.popup__form');
 const profileName = document.querySelector('.profile__name');
 const profileBio = document.querySelector('.profile__bio');
-const nameInput = popup.querySelector('.popup__input_data_name');
-const bioInput = popup.querySelector('.popup__input_data_bio');
-const profileAddButton = document.querySelector('.profile__add-button');
-const elements = document.querySelector('.elements');
-const editingPopup = document.querySelector('.edit-popup');
-const editingPopupCloseButton = editingPopup.querySelector('.popup__close-button-edit');
-const openImagePopup = document.querySelector('.popup-image');
-const popupImageClose = document.querySelector('.popup-image__close-button');
+const nameInput = profilePopup.querySelector('.popup__input_data_name');
+const bioInput = profilePopup.querySelector('.popup__input_data_bio');
 const popupInputName = document.querySelector('.popup__input_name');
 const popupInputURL = document.querySelector('.popup__input_url');
-const editPopupForm = document.querySelector('.edit-popup__form')
+const editPopupForm = document.querySelector('.edit-popup__form');
+// Секция
+const elements = document.querySelector('.elements');
+// Темплейт
+const template = document.querySelector('.template').content;
+const imageContent = document.querySelector('.popup-image__content');
+const popupCaption = document.querySelector('.popup-image__caption')
+// Массив с данными для карточек
 const initialCards = [
   {
     name: 'Архыз',
@@ -45,20 +53,18 @@ const initialCards = [
 
 // ФУНКЦИИ
 // Сохранить форму, добавить новую карточку
-const handleSubmit = e => {
+const handleEditFormSubmit = e => {
   e.preventDefault();
   const name = popupInputName.value;
   const url = popupInputURL.value;
-  popupInputName.value = '';
-  popupInputURL.value = '';
-  elements.append(card(name, url));
-  closeEditingPopup();
+  e.target.reset()
+  elements.prepend(сreateCard(name, url));
+  closePopup(editingPopup);
 }
 
 // Создание карточки посредством клонирования template-тега
 // Также навесил обработчик, чтобы делать кнопку лайка активной при нажатии
-const card = (name, url) => {
-  const template = document.querySelector('.template').content;
+const сreateCard = (name, url) => {
   const templateCard = template.querySelector('.element').cloneNode(true);
   templateCard.querySelector('.element__image').src = url;
   templateCard.querySelector('.element__image').alt = name;
@@ -72,54 +78,65 @@ const card = (name, url) => {
   });
 // Открытие попапа картинки
   const imageButton = templateCard.querySelector('.element__image');
-  const imageContent = document.querySelector('.popup-image__content');
-  const popupCaption = document.querySelector('.popup-image__caption')
   imageButton.addEventListener('click', () => {
     imageContent.src = url;
     imageContent.alt = name;
     popupCaption.textContent = name;
-    openImagePopup.classList.add('popup-image_opened');
+    openPopup(ImagePopup);
   });
-  imageContent.addEventListener('click', () => openImagePopup.classList.remove('popup-image_opened'));
+  imageContent.addEventListener('click', () => closePopup(ImagePopup));
   return templateCard;
 }
 
 // Добавление карточек на страницу из массива initialCards
-const defaultCards = initialCards.forEach(item => elements.prepend(card(item.name, item.url)));
+const defaultCards = initialCards.forEach(item => elements.prepend(сreateCard(item.name, item.url)));
 
 // Отрыктие всплывающего окна, заполнение форм значениями из разметки
-const openPopup = () => {
-  nameInput.value = profileName.textContent;
-  bioInput.value = profileBio.textContent;
+const openPopup = (popup) => {
   popup.classList.add('popup_opened');
 }
 
 // Закрыть всплывающее окно
-const closePopup = () => popup.classList.remove('popup_opened');
+const closePopup = (popup) => popup.classList.remove('popup_opened');
 
 // Открыть всплывающее окно добавления карточки
 const openEditPopup = () => {
-  editingPopup.classList.add('popup_edit_opened');
+  nameInput.value = profileName.textContent;
+  bioInput.value = profileBio.textContent;
+  openPopup(profilePopup)
 }
 
-// Закрыть вспывающее окно
-const closeEditingPopup = () => {
-  editingPopup.classList.remove('popup_edit_opened');
+const closeEditPopup = () => {
+  closePopup(profilePopup);
+}
+
+const openAddPopup = () => {
+  openPopup(editingPopup);
+}
+
+const closeAddPopup = () => {
+  closePopup(editingPopup);
 }
 
 // Сохранить форму, перенести значения из input'а в разметку
-const formSubmitHandler = evt => {
+const handleProfileFormSubmit = evt => {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileBio.textContent = bioInput.value;
-  closePopup();
+  closePopup(profilePopup);
 }
-const closeEditPopup = () => openImagePopup.classList.remove('popup-image_opened');
+
+const closeImagePopup = () => {
+  closePopup(ImagePopup);
+}
+
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
+
 // Обработчики
-profileEditButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-profileAddButton.addEventListener('click', openEditPopup);
-editingPopupCloseButton.addEventListener('click', closeEditingPopup);
-popupForm.addEventListener('submit', formSubmitHandler);
-popupImageClose.addEventListener('click', closeEditPopup);
-editPopupForm.addEventListener('submit', handleSubmit);
+profileEditButton.addEventListener('click', openEditPopup);
+profileAddButton.addEventListener('click', openAddPopup);
+popupForm.addEventListener('submit', handleProfileFormSubmit);
+editPopupForm.addEventListener('submit', handleEditFormSubmit);
